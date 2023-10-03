6d1221782fce42764c1ca255b52d34c8a332e217
pradyunuydarp <pradyundevarakonda24@gmail.com>
2023-10-03 15:07:10 +0530

    message

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -4,7 +4,5 @@
 #include "interpreter.h"
 bool is_arithmetic(char* op);
 long perform_arithmetic(long x, char *op, long y);
-int assignment(char* code, int p_holder,char* o);
-
 
 #endif /* ARITHMETIC_H */
