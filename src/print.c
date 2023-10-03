cefc5d28561337cbb7bf81d24e75d916c98b6ec3
pradyunuydarp <pradyundevarakonda24@gmail.com>
2023-10-03 15:02:07 +0530

    message

--- src/print.c
+++ src/print.c
@@ -1,17 +1,6 @@
 #include "../include/interpreter.h"
 
 void print_variable(char *token){
-
-    // printf("%s",token);
-    // if(token[0]=="){
-
-    // }
-    // char name[10];
-    // int j = 0;
-    // for(; token[j]!=' '; j++){
-    //     name[j]=token[j];
-    // }
-    // name[j+1]='\0';
     for(int i = 0; i<numvars; i++){ 
         if(!strcmp(vars[i]->name,token)){
             printf("%ld\n",vars[i]->val);
