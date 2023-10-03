f470ba37510d16439095142fd0cf515fb032f9cb
pradyunuydarp <pradyundevarakonda24@gmail.com>
2023-10-03 15:07:54 +0530

    message

--- include/variable.h
+++ include/variable.h
@@ -5,4 +5,5 @@ void create_variable(char *token);
 void assign_variable(char *token,int val);
 int get_variable_value(char *var_name);
 void update_value(char *var_name,int x);
+int assignment(char* code, int p_holder,char* o);
 #endif /* VARIABLE_H */ 
